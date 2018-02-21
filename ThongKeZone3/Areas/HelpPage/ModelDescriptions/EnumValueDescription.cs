87e252a62b7ed4eb1a0c8788ebe5a4b3236243f2
Hoang Trung <trung.phan150041@vnuk.edu.vn>
2018-02-21 10:22:51 +0700

    Add project files.

---
+++ ThongKeZone3/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace ThongKeZone3.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ ThongKeZone3/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ThongKeZone3.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
